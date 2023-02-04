9bbc2ba757cae84def99a4045a7c32e6fbe90ddf
rati <ratiruxadzee@gmail.com>
2023-02-04 18:14:21 +0400

    save input data despite of page refreshing using useLocalStorage

--- src/pages/PersonalInfo.tsx
+++ src/pages/PersonalInfo.tsx
@@ -1,9 +1,12 @@
-import React, {useState} from "react";
-import styled from "styled-components";
-import { CaretCircleLeft, Warning, CheckCircle } from "phosphor-react";
+import styled from 'styled-components';
+import {
+  CaretCircleLeft,
+  Warning,
+  CheckCircle,
+} from 'phosphor-react';
 
-import { Link, useNavigate } from "react-router-dom";
-import { useForm } from "react-hook-form";
+import { Link, useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 import {
   Container,
   Content,
@@ -21,20 +24,32 @@ import {
   UploadPhoto,
   WarningMessage,
   WrapperForPhoto,
-} from "../styles/StylesForPages";
-import ResumeContent from "../components/ResumeContent";
+} from '../styles/StylesForPages';
+import ResumeContent from '../components/ResumeContent';
+import { useEffect, useRef } from 'react';
+import useLocalStorage from '../hooks/useLocalStorage';
 
 type Inputs = {
   firstname: string;
   surname: string;
   email: string;
   phone: string;
+  info: string;
 };
-function PersonalInfo() {
-  const [name, setName] = useState("");
-  const [surname, setSurname] = useState("");
-  const [email, setEmail] = useState("");
-  const [phone, setPhone] = useState("");
+
+function PersonalInfo(props: any) {
+  const {
+    name,
+    setName,
+    surname,
+    setSurname,
+    email,
+    setEmail,
+    phone,
+    setPhone,
+    info,
+    setInfo,
+  } = props;
 
   const {
     register,
@@ -42,39 +57,41 @@ function PersonalInfo() {
     formState: { errors },
   } = useForm<Inputs>();
 
-  const navigate = useNavigate();
-
   const onSubmit = () => {
-    navigate("/experience");
+    navigate('/experience');
   };
- 
 
   const handleChange = (event: any, inputName: string) => {
     switch (inputName) {
-      case "firstname":
+      case 'firstname':
         setName(event.target.value);
         break;
-      case "surname":
+      case 'surname':
         setSurname(event.target.value);
         break;
-      case "email":
+      case 'info':
+        setInfo(event.target.value);
+        break;
+      case 'email':
         setEmail(event.target.value);
         break;
-      case "phone":
+      case 'phone':
         setPhone(event.target.value);
         break;
       default:
         break;
     }
   };
-  function validateGeorgian(value: string): boolean | string {
+  const validateGeorgian = (value: string): boolean | string => {
     const pattern = /^[\u10A0-\u10FF]+$/;
-    return pattern.test(value) || "";
-  }
-   const validateGeorgianPhone = (value: string) => {
-     const phoneRegex = /^(\+995|00995|995)?(5[0-9]{8})$/;
-     if (!phoneRegex.test(value)) return false;
-     return true;
+    return pattern.test(value) || '';
+  };
+  const validateGeorgianPhone = (value: any) => {
+    // const phoneRegex = /^(\+995|00995|995)?(5[0-9]{8})$/;
+    const phoneRegex =
+      /^(\+995\s)?(5[0-9]{2}\s[0-9]{2}\s[0-9]{2}\s[0-9]{2})$/;
+    if (!phoneRegex.test(value)) return false;
+    return true;
   };
   const validateEmail = (value: string) => {
     const emailRegex = /^\w+([.-]?\w+)*@redberry.ge$/;
@@ -82,12 +99,43 @@ function PersonalInfo() {
     return true;
   };
 
+  const hiddenFileInput = useRef<HTMLInputElement>(null as any);
+
+  const handleClick = () => {
+    hiddenFileInput.current.click();
+  };
+  const handleChangeInput = (event: any) => {
+    const fileUploaded = event.target.files[0];
+    props.handleFile(fileUploaded);
+  };
+
+  const navigate = useNavigate();
+
+
+  useEffect(() => {
+    return () => {
+      window.localStorage.clear();
+    };
+  }, [navigate]);
+
+  const clearStorage = () => {
+    setName('');
+    setSurname('');
+    setEmail('');
+    setInfo('');
+    setPhone('');
+    navigate('/');
+  };
+
   return (
-    <div style={{ display: "flex" }}>
+    <div style={{ display: 'flex' }}>
       <Container>
-        <Link to="/">
-          <CaretCircleLeft size={38} style={{ color: "black" }} />
-        </Link>
+        <CaretCircleLeft
+          size={38}
+          style={{ color: 'black' }}
+          onClick={clearStorage}
+        />
+
         <Content>
           <Wrapper>
             <Header>პირადი ინფო</Header>
@@ -97,81 +145,132 @@ function PersonalInfo() {
           <ForInputs>
             <FormContainer>
               <Label>სახელი</Label>
-              <Input
-                type="text"
-                placeholder="ანზორ"
-                maxLength={28}
-                style={{
-                  border: errors.firstname ? "1px solid red" : "",
-                }}
-                {...register("firstname", {
-                  required: true,
-                  minLength: 2,
-                  validate: validateGeorgian,
-                })}
-                onChange={(event) => handleChange(event, "firstname")}
-              ></Input>
-              <WarningMessage>მინიმუმ 2 ასო, ქართული ასოები</WarningMessage>
+              <FormInput>
+                <Input
+                  type="text"
+                  placeholder="ანზორ"
+                  maxLength={14}
+                  value={name}
+                  style={{
+                    border: errors.firstname ? '1px solid red' : '',
+                  }}
+                  {...register('firstname', {
+                    required: true,
+                    minLength: 2,
+                    validate: validateGeorgian,
+                  })}
+                  onChange={(event) =>
+                    handleChange(event, 'firstname')
+                  }
+                ></Input>
+                <Error>
+                  {errors.firstname && (
+                    <Warning size={16} color="red" />
+                  )}
+                </Error>
+              </FormInput>
+              <WarningMessage>
+                მინიმუმ 2 ასო, ქართული ასოები
+              </WarningMessage>
             </FormContainer>
             <FormContainer>
               <Label>გვარი</Label>
-              <Input
-                type="text"
-                placeholder="მუმლაძე"
-                style={{
-                  border: errors.surname ? "1px solid red" : "",
-                }}
-                {...register("surname", {
-                  required: true,
-                  minLength: 2,
-                  validate: validateGeorgian,
-                })}
-                onChange={(event) => handleChange(event, "surname")}
-              ></Input>
-              <WarningMessage>მინიმუმ 2 ასო, ქართული ასოები</WarningMessage>
+              <FormInput>
+                <Input
+                  type="text"
+                  placeholder="მუმლაძე"
+                  maxLength={20}
+                  value={surname}
+                  style={{
+                    border: errors.surname ? '1px solid red' : '',
+                  }}
+                  {...register('surname', {
+                    required: true,
+                    minLength: 2,
+                    validate: validateGeorgian,
+                  })}
+                  onChange={(event) => handleChange(event, 'surname')}
+                ></Input>
+                <Error>
+                  {errors.firstname && (
+                    <Warning size={16} color="red" />
+                  )}
+                </Error>
+              </FormInput>
+              <WarningMessage>
+                მინიმუმ 2 ასო, ქართული ასოები
+              </WarningMessage>
             </FormContainer>
           </ForInputs>
           <WrapperForPhoto>
-            <Label style={{ fontSize: "18px" }}>პირადი ფოტოს ატვირთვა</Label>
-            <UploadPhoto>ატვირთვა</UploadPhoto>
+            <Label style={{ fontSize: '18px' }}>
+              პირადი ფოტოს ატვირთვა
+            </Label>
+            <UploadPhoto onClick={handleClick}>ატვირთვა</UploadPhoto>
+            <InputForPhotoUpload
+              type="file"
+              ref={hiddenFileInput}
+              onChange={handleChangeInput}
+              style={{ display: 'none' }}
+            />
           </WrapperForPhoto>
           <AnotherWrapper>
             <Label>ჩემ შესახებ (არასავალდებულო)</Label>
-            <TextArea placeholder="ზოგადი ინფო შენ შესახებ"></TextArea>
+            <TextArea
+              placeholder="ზოგადი ინფო შენ შესახებ"
+              name="info"
+              value={info}
+              onChange={(event) => handleChange(event, 'info')}
+            ></TextArea>
           </AnotherWrapper>
           <AnotherWrapper>
             <Label>ელ.ფოსტა</Label>
-            <Input
-              type="email"
-              placeholder="anzor666@redberry.ge"
-              style={{
-                width: "100%",
-                border: errors.email ? "1px solid red" : "",
-              }}
-              {...register("email", {
-                required: true,
-                minLength: 2,
-                validate: validateEmail,
-              })}
-              onChange={(event) => handleChange(event, "email")}
-            ></Input>
-            <WarningMessage>უნდა მთავრდებოდეს @redberry.ge-ით</WarningMessage>
+            <FormInput>
+              <Input
+                type="email"
+                placeholder="anzor666@redberry.ge"
+                value={email}
+                style={{
+                  width: '100%',
+                  border: errors.email ? '1px solid red' : '',
+                }}
+                {...register('email', {
+                  required: true,
+                  minLength: 2,
+                  validate: validateEmail,
+                })}
+                onChange={(event) => handleChange(event, 'email')}
+              ></Input>
+              <Error>
+                {errors.email && <Warning size={16} color="red" />}
+              </Error>
+            </FormInput>
+            <WarningMessage>
+              უნდა მთავრდებოდეს @redberry.ge-ით
+            </WarningMessage>
           </AnotherWrapper>
           <AnotherWrapper>
             <Label>მობილურის ნომერი</Label>
-            <Input
-              type="tel"
-              placeholder="+995 551 12 34 56"
-              style={{
-                width: "100%",
-                border: errors.phone ? "1px solid red" : "",
-              }}
-              {...register("phone", {
-                required: true,
-                validate: validateGeorgianPhone,
-              })}
-              onChange={(event) => handleChange(event, "phone")}
-            ></Input>
+            <FormInput>
+              <Input
+                type="tel"
+                placeholder="+995 551 12 34 56"
+                value={phone}
+                style={{
+                  width: '100%',
+                  border: errors.phone ? '1px solid red' : '',
+                }}
+                {...register('phone', {
+                  required: true,
+                  minLength: 2,
+                  validate: validateGeorgianPhone,
+                })}
+                onChange={(event) => handleChange(event, 'phone')}
+              ></Input>
+              <Error>
+                {errors.phone && <Warning size={16} color="red" />}
+              </Error>
+            </FormInput>
             <WarningMessage>
               უნდა აკმაყოფილებდეს ქართული მობილურის ნომრის ფორმატს
             </WarningMessage>
@@ -186,6 +285,7 @@ function PersonalInfo() {
         surname={surname}
         email={email}
         phone={phone}
+        info={info}
       />
     </div>
   );
@@ -198,3 +298,20 @@ const WrapperForButton = styled(AnotherWrapper)`
   margin-top: 61px;
 `;
 
+const FormInput = styled.form`
+  width: 100%;
+  height: 48px;
+  border-radius: 4px;
+  background: white;
+  display: flex;
+  align-items: center;
+  position: relative;
+  flex-direction: row-reverse;
+`;
+
+const Error = styled.div`
+  margin-right: 12px;
+  position: absolute;
+`;
+
+const InputForPhotoUpload = styled.input``;
