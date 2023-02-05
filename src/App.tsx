9c1cecfe9ff851a7c7812af88b0f144366f228bc
RatiRukhadze <ratiruxadzee@gmail.com>
2023-02-06 01:15:31 +0400

    refactor elements in the App component and update Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,129 @@
-import { useState } from 'react';
-import useLocalStorage  from './hooks/useLocalStorage';
+import { useState } from "react";
+import useLocalStorage from "./hooks/useLocalStorage";
 
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link,
-} from 'react-router-dom';
-import { GlobalStyles } from './styles/GlobalStyles';
-import Home from './components/Home';
-import PersonalInfo from './pages/PersonalInfo';
-import Experience from './pages/Experience';
-import Education from './pages/Education';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { GlobalStyles } from "./styles/GlobalStyles";
+import Home from "./components/Home";
+import PersonalInfo from "./pages/PersonalInfo";
+import Experience from "./pages/Experience";
+import Education from "./pages/Education";
+import ResumeContent from "./components/ResumeContent";
+import { InputContext } from "./contexts/InputContext";
 function App() {
-  const [name, setName] = useLocalStorage('name','');
-  const [surname, setSurname] = useLocalStorage( 'surname', '');
-  const [info, setInfo] = useLocalStorage('info','');
-  const [email, setEmail] = useLocalStorage('email','');
-  const [phone, setPhone] = useLocalStorage('phone','');
+  //for personal info
+  const [name, setName] = useLocalStorage("name", "");
+  const [surname, setSurname] = useLocalStorage("surname", "");
+  const [info, setInfo] = useLocalStorage("info", "");
+  const [email, setEmail] = useLocalStorage("email", "");
+  const [phone, setPhone] = useLocalStorage("phone", "");
+  const [image, setImage] = useLocalStorage("image", "");
+  const [showImage, setShowImage] = useLocalStorage("display", false);
+  //for experience
+  const [position, setPosition] = useLocalStorage("position", "");
+  const [employer, setEmployer] = useLocalStorage("employer", "");
+
   return (
-    <div>
-      <GlobalStyles />
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route
-            path="/personal"
-            element={
-              <PersonalInfo
-                name={name}
-                setName={setName}
-                surname={surname}
-                setSurname={setSurname}
-                email={email}
-                setEmail={setEmail}
-                phone={phone}
-                setPhone={setPhone}
-                info={info}
-                setInfo={setInfo}
-              />
-            }
-          />
-          <Route path="/experience" element={<Experience />} />
-          <Route path="/education" element={<Education />} />
-        </Routes>
-      </Router>
-    </div>
+    <InputContext.Provider
+      value={{
+        name,
+        surname,
+        info,
+        email,
+        phone,
+        image,
+        showImage,
+        setEmail,
+        setImage,
+        setInfo,
+        setName,
+        setPhone,
+        setSurname,
+        setShowImage,
+      }}
+    >
+      <div style={{ display: "flex" }}>
+        <GlobalStyles />
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route
+              path="/personal"
+              element={[
+                <PersonalInfo
+                  name={name}
+                  setName={setName}
+                  surname={surname}
+                  setSurname={setSurname}
+                  email={email}
+                  setEmail={setEmail}
+                  phone={phone}
+                  setPhone={setPhone}
+                  info={info}
+                  setInfo={setInfo}
+                  image={image}
+                  setImage={setImage}
+                  showImage={showImage}
+                  setShowImage={setShowImage}
+                />,
+                <ResumeContent
+                  name={name}
+                  surname={surname}
+                  email={email}
+                  phone={phone}
+                  info={info}
+                  image={image}
+                  setImage={setImage}
+                  showImage={showImage}
+                  position={position}
+                  employer={employer}
+                />,
+              ]}
+            />
+            <Route
+              path="/experience"
+              element={[
+                <Experience
+                  position={position}
+                  setPosition={setPosition}
+                  employer={employer}
+                  setEmployer={setEmployer}
+                />,
+                <ResumeContent
+                  name={name}
+                  surname={surname}
+                  email={email}
+                  phone={phone}
+                  info={info}
+                  image={image}
+                  setImage={setImage}
+                  showImage={showImage}
+                  position={position}
+                  employer={employer}
+                />,
+              ]}
+            />
+            <Route
+              path="/education"
+              element={[
+                <Education />,
+                <ResumeContent
+                  name={name}
+                  surname={surname}
+                  email={email}
+                  phone={phone}
+                  info={info}
+                  image={image}
+                  setImage={setImage}
+                  showImage={showImage}
+                  position={position}
+                  employer={employer}
+                />,
+              ]}
+            />
+          </Routes>
+        </Router>
+      </div>
+    </InputContext.Provider>
   );
 }
 
