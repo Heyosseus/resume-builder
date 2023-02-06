6d3d2b7282f92a913f5496f77a56f0cbd945db4f
RatiRukhadze <ratiruxadzee@gmail.com>
2023-02-06 22:56:08 +0400

    retrieve data from api and store this data into select form

--- src/pages/Education.tsx
+++ src/pages/Education.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import styled from 'styled-components';
-import { CaretCircleLeft } from 'phosphor-react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { CaretCircleLeft } from "phosphor-react";
+import { Link, useNavigate } from "react-router-dom";
 import {
   Container,
   Content,
@@ -16,14 +16,98 @@ import {
   TextArea,
   WarningMessage,
   Toggle,
-} from '../styles/StylesForPages';
-import ResumeContent from '../components/ResumeContent';
-function Education() {
+} from "../styles/StylesForPages";
+
+interface Option {
+  value: string;
+  text: string;
+  id: number;
+  title: string;
+}
+
+function Education(props: any) {
+  const {
+    position,
+    employer,
+    handleChange,
+    setStartDate,
+    setEndDate,
+    setExperience,
+    handleAddInput,
+    inputs,
+    setName,
+    setSurname,
+    setEmail,
+    setPhone,
+    setInfo,
+    setImage,
+    setPosition,
+    setEmployer,
+    school,
+    setSchool,
+    degree,
+    setDegree,
+    endOfStudy,
+    setEndOfStudy,
+    bio,
+    setBio,
+    display,
+    setDisplay,
+  } = props;
+
+  const navigate = useNavigate();
+
+  const clearStorageForEdu = () => {
+    setStartDate("");
+    setEndDate("");
+    setPosition("");
+    setName("");
+    setSurname("");
+    setEmail("");
+    setPhone("");
+    setInfo("");
+    setEmployer("");
+    setExperience("");
+    navigate("/");
+  };
+
+  const handleFinish = () => {
+    setDisplay(!display);
+  };
+
+  const endpoint = "https://resume.redberryinternship.ge/api/degrees";
+
+  const [options, setOptions] = useState<Option[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(endpoint, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+        const data = await response.json();
+        setOptions(data);
+        console.log(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
   return (
     <div>
       <Container>
         <Link to="/">
-          <CaretCircleLeft size={38} style={{ color: 'black' }} />
+          <CaretCircleLeft
+            size={38}
+            style={{ color: "black" }}
+            onClick={clearStorageForEdu}
+          />
         </Link>
         <Content>
           <Wrapper>
@@ -37,7 +121,11 @@ function Education() {
               <Input
                 type="text"
                 placeholder="სასწავლებელი"
-                style={{ width: '100%' }}
+                value={school}
+                style={{ width: "100%" }}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                  handleChange(event, "school")
+                }
               ></Input>
               <WarningMessage>მინიმუმ 2 სიმბოლო</WarningMessage>
             </FormContainer>
@@ -45,30 +133,49 @@ function Education() {
           <ForDates>
             <AnotherWrapper>
               <Label>ხარისხი</Label>
-              <Input type="" placeholder="აირჩიეთ ხარისხი"></Input>
+              <Select
+                onChange={(event: React.ChangeEvent<HTMLSelectElement>) =>
+                  handleChange(event, "degree")
+                }
+              >
+                {options.map((option) => (
+                  <Option key={option.id} value={option.title}>
+                    {option.title}
+                  </Option>
+                ))}
+              </Select>
             </AnotherWrapper>
             <AnotherWrapper>
               <Label>დამთავრების რიცხვი</Label>
-              <Input type="date"></Input>
+              <Input
+                type="date"
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                  handleChange(event, "endOfStudy")
+                }
+              ></Input>
             </AnotherWrapper>
           </ForDates>
           <AnotherWrapper>
             <Label>აღწერა</Label>
             <TextArea
               placeholder="განათლების აღწერა"
-              style={{ height: '179px' }}
+              style={{ height: "179px" }}
+              value={bio}
+              onChange={(event: React.ChangeEvent<HTMLTextAreaElement>) =>
+                handleChange(event, "bio")
+              }
             ></TextArea>
           </AnotherWrapper>
           <AnotherWrapper>
-            <Line style={{ background: '#C1C1C1' }}></Line>
+            <Line style={{ background: "#C1C1C1" }}></Line>
           </AnotherWrapper>
           <Button>მეტი გამოცდილების დამატება</Button>
           <ButtonContainer>
             <Link to="/experience">
               <Toggle>უკან</Toggle>
             </Link>
-            <Link to="/education">
-              <Toggle>შემდეგი</Toggle>
+            <Link to="/finish">
+              <Toggle onClick={handleFinish}>დასრულება</Toggle>
             </Link>
           </ButtonContainer>
         </Content>
@@ -114,3 +221,21 @@ const Button = styled.button`
     cursor: pointer;
   }
 `;
+
+
+const Select = styled.select`
+  width: 371px;
+  height: 48px;
+  align-items: center;
+  display: flex;
+  outline: none;
+  border: 1px solid #c1c1c1;
+  padding-left: 12px;
+`
+
+const Option = styled.option`
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 21px;
+  color: black;
+`
